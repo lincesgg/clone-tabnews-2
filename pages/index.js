73212559b3faf419097429ff54a86a0fa31d456d
lincesgg <87877330+lincesgg@users.noreply.github.com>
2024-04-28 02:11:50 +0000

    feat(app) Adding Index Page

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function home(){
+    return (
+        <div>
+            <h1>Você já pensou em um site estar falando com você?</h1>
+            <h2>Cerejuu 😏</h2>
+        </div>
+    );
+}
+
+export default home;
