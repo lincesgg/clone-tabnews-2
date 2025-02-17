278c14920e25a0a0646dc3a0b64d4f660e9d2c6c
lincesgg <lincesgg@gmail.com>
2025-02-17 09:16:15 -0300

    Update api/v1/status To Provide Real Data

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,28 @@
 import database from "#infra/database.js";
 
 async function status(req, res) {
-  const a = await database.query("SELECT 1 + 1;");
-  console.log(a.rows);
+  const updateAt = new Date().toISOString();
+
+  const dbVersionRes = await database.query("SHOW server_version;");
+  const dbVersion = dbVersionRes.rows[0].server_version;
+
+  const dbMaxConnectionsRes = await database.query("SHOW max_connections;");
+  const dbMaxConnections = dbMaxConnectionsRes.rows[0].max_connections;
+
+  const dbConnectionsRes = await database.query(
+    `SELECT count(*)::int FROM pg_stat_activity WHERE datname = '${process.env.POSTGRES_DB}';`,
+  );
+  const dbConnections = dbConnectionsRes.rows[0].count;
+
   res.status(200).json({
-    request: "Recebido!",
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: dbVersion,
+        max_connections: parseInt(dbMaxConnections),
+        crr_connections: parseInt(dbConnections),
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,4 +3,22 @@ const host = "http://localhost:3000";
 test("GET request '/api/v1/status' should return 200!", async () => {
   const res = await fetch(`${host}//api/v1/status`);
   expect(res.status).toBe(200);
+
+  const resBody = await res.json();
+  const databaseBody = resBody.dependencies.database;
+
+  // .update_at Pode ser Convertido Para uma Data
+  // Essa Data ela deve ser explicitamente representada update_at (Não explicito: null → 1970 0:0)
+  const parsedDate = new Date(resBody.update_at);
+  expect(parsedDate.toISOString()).toEqual(resBody.update_at);
+
+  // databaseBody.version deve estar no modelo "16.6", na mesma versão que a versão de fato do postgres
+  expect(databaseBody.version).toEqual("16.6");
+
+  // Connections (Amount) must Be Int
+  expect(Number.isInteger(databaseBody.max_connections)).toEqual(true);
+  expect(Number.isInteger(databaseBody.crr_connections)).toEqual(true);
+
+  // If There's More that 1 crr_connections per time, then there is connections leakage
+  expect(databaseBody.crr_connections).toBe(1);
 });
