bf6483de2072eb0182a1e8687a9e8ab7de6a00ed
lincesgg <lincesgg@gmail.com>
2025-04-03 14:24:09 -0300

    [feat] Implement Query Sanitization `api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -9,9 +9,11 @@ async function status(req, res) {
   const dbMaxConnectionsRes = await database.query("SHOW max_connections;");
   const dbMaxConnections = dbMaxConnectionsRes.rows[0].max_connections;
 
-  const dbConnectionsRes = await database.query(
-    `SELECT count(*)::int FROM pg_stat_activity WHERE datname = '${process.env.POSTGRES_DB}';`,
-  );
+  const dbConnectionsRes = await database.query({
+    text: `SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+    values: [process.env.POSTGRES_DB],
+  });
+
   const dbConnections = dbConnectionsRes.rows[0].count;
 
   res.status(200).json({
