3d368deb1293c207b0866c51cf69d6b530e32ca2
lincesgg <lincesgg@gmail.com>
2025-04-04 00:17:35 -0300

    [feat] Allow For SSL Connections

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,7 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
     port: process.env.POSTGRES_PORT,
+    ssl: getSSLValues(),
   });
 
   try {
@@ -14,7 +15,7 @@ async function query(queryObject) {
     const response = await client.query(queryObject);
     return response;
   } catch (err) {
-    console.log(`@ '${queryObject}': ${err}`);
+    console.log("@", queryObject, `→ ${err}`);
     throw err;
   } finally {
     try {
@@ -28,3 +29,13 @@ async function query(queryObject) {
 export default {
   query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV == "development" ? false : true;
+}
