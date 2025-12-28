dac24b8348ddf9335606c1bdcfeb7258c0eeb6c2
Lince <licesgg@gmail.com>
2025-12-28 00:21:52 -0300

    [Refactor] Rewritten `wait-for-db.js` using `async-retry`

--- infra/scripts/wait-for-db.js
+++ infra/scripts/wait-for-db.js
@@ -1,41 +1,43 @@
 const { exec } = require("node:child_process")
+const retry = require("async-retry")
 
-waitForDatabaseAvalaibility()
+waitForDB()
 
-async function waitForDatabaseAvalaibility() {
-
-	let isDbAvailable = await isDatabaseAvailable()
-
-	if (!isDbAvailable) {
-		process.stdout.write("\n🔴 Database Unavailable, ⌛ Waiting for Availability ")
-	}
-
-	while (!isDbAvailable) {
-		isDbAvailable = await isDatabaseAvailable()
-		process.stdout.write(".")
-		await sleep(150)
-	}
-
+async function waitForDB() {
+	let firstIt = true
+	await retry(
+		isDatabaseAvailable,
+		{
+			retries: 150,
+			minTimeout: 200,
+			maxTimeout: 2500,
+			factor: 1.05,
+			onRetry: (err) => {
+				if (firstIt) {
+					process.stdout.write("\n🔴 Database Unavailable, ⌛ Waiting for Availability ")
+					firstIt = false
+				}
+				process.stdout.write(".")
+			}
+		}
+	)
 	console.log("\n🟢 Database is Available for Connections!")
-	return 0
+
 }
 
-function isDatabaseAvailable() {
-	return new Promise((res, rej) => {
-		exec("docker exec postgres-dev pg_isready -h localhost",
-			(err, stdout) => {
-				ret = stdout.includes("accepting connections")
+async function isDatabaseAvailable(bail) {
+	const availability = new Promise((res, rej) => {
+		exec("docker exec postgres-dev pg_isready -h localhost", (err, stdout) => {
+			const ret = stdout.includes("accepting connections")
+
+			if (ret) {
 				res(ret)
-			})
+			}
+			else {
+				rej("DB Unavailable")
+			}
+		})
 	})
+
+	await availability
 }
-
-function sleep(ms) {
-	return new Promise((res, rej) => {
-		setTimeout(() => {
-			res()
-		}, ms)
-	})
-}
-
-
